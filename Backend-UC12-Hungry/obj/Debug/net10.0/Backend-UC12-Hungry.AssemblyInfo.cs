1d1f7eee69a13361cc0ad75555058bd4ef8426bc
JerryFilho <jerryfilho123@gmail.com>
2026-08-12 20:16:18 -0300

    Conexao git ignore

--- Backend-UC12-Hungry/obj/Debug/net10.0/Backend-UC12-Hungry.AssemblyInfo.cs
+++ Backend-UC12-Hungry/obj/Debug/net10.0/Backend-UC12-Hungry.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend-UC12-Hungry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f420fc22412eb5f210ce97feaeea02c3ef37c7c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend-UC12-Hungry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend-UC12-Hungry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
